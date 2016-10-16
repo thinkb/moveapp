697fe212313cafe165030e3aa6e704e8ad137608
famarcon <famarcon@br.ibm.com>
2016-10-16 19:21:10 -0200

    New final template using Node

---
+++ moveapplication/app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var cfenv = require('cfenv');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+var db = [];
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser());
+
+var appEnv = cfenv.getAppEnv();
+
+var router = express.Router();
+
+router.route('/users')
+  .get(function(req, res){
+
+    res.json({json: db});
+
+  }).post(function(req, res){
+    console.log('post');
+
+    db.push({name:'haha'});
+
+    res.json({message: "Usuario cadastrado com sucesso"});
+  });
+
+
+app.use('/move', router);
+app.listen(appEnv.port, '0.0.0.0', function() {
+  console.log("server starting on " + appEnv.url);
+});
