f5a08384e57e63cfcc886bb51197d8db8833ced7
Bruna Andrade <Bruna Andrade>
2016-10-18 17:23:55 -0200

    pag principal, amigos e desafios.

---
+++ moveapplication/public/js/minha_area.js
@@ -0,0 +1,16 @@
+$('#amigos-pag').click(function(){
+  $('#main-intern-div').empty();
+  $('#main-intern-div').load('amigos.html');
+});
+
+
+$('#desafios-pag').click(function(){
+  $('#main-intern-div').empty();
+  $('#main-intern-div').load('desafios.html');
+});
+
+
+$('#conquistas-pag').click(function(){
+  $('#main-intern-div').empty();
+  $('#main-intern-div').load('conquistas.html');
+});
