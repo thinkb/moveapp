a2adcc1a52ee68f342f30de8c9855a331e3ad841
Bruna Andrade <Bruna Andrade>
2016-11-09 00:47:33 -0200

    pag adm

--- moveapplication/public/js/index.js
+++ moveapplication/public/js/index.js
@@ -1,8 +1,28 @@
 var user;
-
+var keykey = 0 ;
 $('#beginRegister').click(function(){
   $('#myMainDiv').empty();
   $('#myMainDiv').load('register.html');
+});
+
+
+$('#key').click(function(){
+if (keykey == 0) {
+  $('#logindiv').empty();
+  $('#logindiv').load('login_pais.html');
+  $('body').removeClass('bgblue');
+  $('body').addClass('bgpurple');
+  keykey++;
+}
+else {
+  if (keykey == 1) {
+    $('#myMainDiv').empty();
+    $('#myMainDiv').load('login_user.html');
+    $('body').addClass('bgblue');
+    $('body').removeClass('bgpurple');
+    keykey==0;
+  }
+}
 });
 
 $('#loginButton').click(function(){
---
+++ moveapplication/public/js/loginadm.js
@@ -0,0 +1,7 @@
+
+$('#loginadm').click(function(){
+  alert("oi");
+  $('#myMainDiv').empty();
+  $('#myMainDiv').load('adm_area.html');
+  $('body').removeClass('bgpurple');
+});
