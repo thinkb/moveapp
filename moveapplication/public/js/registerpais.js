ca2a841900d7e01ae530eb3549b8cea511fb04b3
famarcon <famarcon@br.ibm.com>
2016-11-19 19:20:32 -0200

    form validationadm

--- moveapplication/public/js/registerpais.js
+++ moveapplication/public/js/registerpais.js
@@ -1,18 +1,30 @@
 $("#cadastropais").click(function(){
 
-  var p = {
-    namepai: $('#namepai').val(),
-    lastpai: $('#lastpai').val(),
-    emailpai: $('#emailpai').val(),
-    pwdpai: $('#pwdpai').val()
-  };
-
-  $.post( "/move/pais", p, function( data ) {
-    //$('#sucesso').openModal();
-
-    alert('success!');
-
-  }, "json");
+  var np = $('#namepai').val() == "";
+  var lp = $('#lastpai').val() == "";
+  var ep = $('#emailpai').val() == "";
+  var pp = $('#pwdpai').val() == "";
+  var p2p = $('#pwd2pai').val() == "";
 
 
-})
+  if(np || lp || ep || pp || p2p){
+    //nada
+  } else {
+
+    var p = {
+      namepai: $('#namepai').val(),
+      lastpai: $('#lastpai').val(),
+      emailpai: $('#emailpai').val(),
+      pwdpai: $('#pwdpai').val()
+    };
+
+    $.post( "/move/pais", p, function( data ) {
+      //$('#sucesso').openModal();
+
+      alert('success!');
+
+    }, "json");
+
+  }
+
+});
